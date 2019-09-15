93e96bd96fadc5c7de3039e42435944b99c0f8b9
ksk-yoshiura <mrtambourineman8358@gmail.com>
2019-09-15 23:03:08 +0900

    Login and signed up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: :true
   has_many :books, dependent: :destroy
   has_many :chains, dependent: :destroy
   has_many :likes, dependent: :destroy
