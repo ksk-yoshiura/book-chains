9ebb0c1ed008a9af934004bc0ecf25c774de2ea2
ksk-yoshiura <mrtambourineman8358@gmail.com>
2019-09-16 20:38:00 +0900

    make it better

--- db/migrate/20190715042100_create_book_shelves.rb
+++ db/migrate/20190715042100_create_book_shelves.rb
@@ -1,10 +1,12 @@
 class CreateBookShelves < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :book_shelves do |t|
       t.integer :user_id
       t.integer :book_id
 
       t.timestamps
     end
+  end
+  def down
   end
 end
