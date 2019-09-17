7391749163b9bed09b038c1b2149d357ebd0985a
ksk-yoshiura <mrtambourineman8358@gmail.com>
2019-09-17 13:09:54 +0900

    Like function with async
    completed!!

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,19 +4,26 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, chain_id: params[:chain_id])
     @likes = Like.where(chain_id: params[:chain_id])
-    @chain.reload
+    arr = [@set_chain]
+    chain_arr = Chain.chain_arr_make(arr)
+    @chain = chain_arr[0]
+    # @chain.reload
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, chain_id: params[:chain_id])
+    like = Like.find_by(user_id: current_user.id, chain_id: params[:chain_id])
     like.destroy
     @likes = Like.where(chain_id: params[:chain_id])
-    @chain.reload
+    arr = [@set_chain]
+    chain_arr = Chain.chain_arr_make(arr)
+    @chain = chain_arr[0]
+    
+    # @chain.reload
   end
 
   private
 
   def set_chain
-    @chain = Chain.find(params[:chain_id])
+    @set_chain = Chain.find(params[:chain_id])
   end
 end
