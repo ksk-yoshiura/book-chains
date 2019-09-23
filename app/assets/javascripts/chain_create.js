2cd62e9e5d587af55a33e7f919d9ec99d06f7601
ksk-yoshiura <mrtambourineman8358@gmail.com>
2019-09-23 22:44:50 +0900

    chains create on new page
    editted card design

--- app/assets/javascripts/chain_create.js
+++ app/assets/javascripts/chain_create.js
@@ -8,21 +8,20 @@ $(document).on("click", ".new-chain-btn-add", function () {
   var bookAuthor = $(this).data('book-author');
   var bookImage = $(this).data('book-image');
   var bookCreated_at = $(this).data('book-created_at');
-  var addHTML = `<div class="card" style="min-width:20%; max-width:20%">
-  <img class="card-img-top" src="${bookImage}" alt="">
-    <div class="card-body">
-      <h4 class="card-title">${bookTitle}</h4>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item"><p>著者 : ${bookAuthor}</P></li>
-        <li class="list-group-item"><p>登録日 : ${bookCreated_at}</P></li>
-      </ul>
-      <button class="btn btn-danger new-chain-btn-remove" 
-      data-book-id="${bookId}" data-book-title="${bookTitle}" 
-      data-book-author="${bookAuthor}" data-book-image="${bookImage}" 
-      data-book-created_at="${bookCreated_at}" >削除</button>
-    </div>
-    <input type="hidden" name="chain[book_ids][]" value="${bookId}" />
-    </div>`;
+  var addHTML = 
+        `<li class="card" style="min-width:20%; max-width:20%" >
+          <img class="card-img-top" src="${bookImage}" alt="" >
+            <div class="card-body">
+              <p class="card-title">${bookTitle}</p>
+              <p>著者 : ${bookAuthor}</P>
+              
+              <button class="btn btn-danger new-chain-btn-remove" 
+                data-book-id="${bookId}" data-book-title="${bookTitle}" 
+                data-book-author="${bookAuthor}" data-book-image="${bookImage}" 
+                data-book-created_at="${bookCreated_at}" >削除</button>
+            </div>
+          <input type="hidden" name="chain[book_ids][]" value="${bookId}">
+        </li>`;
   $("#make-new-chain").append(addHTML);
 });
 
@@ -34,20 +33,20 @@ $(document).on("click", ".new-chain-btn-remove", function () {
   var bookAuthor = $(this).data('book-author');
   var bookImage = $(this).data('book-image');
   var bookCreated_at = $(this).data('book-created_at');
-  var returnHTML = `<div class="card" style="min-width:20%; max-width:20%">
-  <img class="card-img-top" src="${bookImage}" alt="">
-    <div class="card-body">
-      <h4 class="card-title">${bookTitle}</h4>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item"><p>著者 : ${bookAuthor}</P></li>
-        <li class="list-group-item"><p>登録日 : ${bookCreated_at}</P></li>
-      </ul>
-      <button class="btn btn-primary new-chain-btn-add" 
-      data-book-id="${bookId}" data-book-title="${bookTitle}" 
-      data-book-author="${bookAuthor}" data-book-image="${bookImage}" 
-      data-book-created_at="${bookCreated_at}">追加</button>
-    </div>
-    </div>`;
+  var returnHTML =
+        `<li class="card" style="min-width:20%; max-width:20%">
+          <img class="card-img-top" src="${bookImage}" alt="" >
+            <div class="card-body">
+              <p class="card-title">${bookTitle}</p>
+              <p>著者 : ${bookAuthor}</P>
+              <p>登録日 : <br>${bookCreated_at}</P>
+              
+              <button class="btn btn-primary new-chain-btn-add" 
+              data-book-id="${bookId}" data-book-title="${bookTitle}"
+              data-book-author="${bookAuthor}" data-book-created_at="${bookCreated_at}"
+              data-book-image="${bookImage}">追加</button>
+            </div>
+        </li>`;
   $("#bookshelf").append(returnHTML);
 });
 });
